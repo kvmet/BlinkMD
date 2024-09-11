fd9280fe8147306d73fa5f039fa9e2f480aa2305
Kristen Metcalfe <kmet28@gmail.com>
2024-09-11 16:01:24 -0400

    Add advanced features. Minor js cleanup

--- blinkmd.js
+++ blinkmd.js
@@ -1,22 +1,22 @@
 {
-  addEventListener("load", (event) => {
+  addEventListener(`load`, (event) => {
     const regex = /[^\w-\d()/]/g; // Regex to clean url query
-    sitePath = "/../site/"; // Relative to the location of your index.html
+    sitePath = `/`;
+    headerFile = null; // TODO
+    footerFile = null; // TODO
 
-    // TODO: import header and footer files?
+    if (sitePath[0] == `/`) {
+      baseUrl = window.location.href;
+      sitePath = baseUrl.substring(0, baseUrl.lastIndexOf(`/`)) + sitePath;
+    }
 
     // TODO: strip key-value pairs? media format types?
-    path = window.location.search.replace(regex,"");
+    path = window.location.search.replace(regex,``);
 
     if(path.length <= 1) {
       path = "index.md";
     } else {
       path += ".md";
-    }
-    if(sitePath[0] == `/`) {
-      path = window.location.protocol + `//` + window.location.pathname + sitePath + path;
-    } else {
-      path = sitePath + path;
     }
 
     const el = document.querySelector("content");
@@ -34,9 +34,8 @@
 
         console.log(converter.getMetadata());
 
+        //TODO: I suspect this is double-parsing because makeHtml returns a string. could maybe be more performant?
         el.innerHTML = converter.makeHtml(markdown);
-
-        //el.replaceChildren(converter.makeHtml(markdown));
       })
       .catch((error) => {
         el.replaceChildren(`Failed to load : ${error}`);
