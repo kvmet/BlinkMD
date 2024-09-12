ef623cd102ba172b1be4179debbe5976eec1ea65
Kristen Metcalfe <kmet28@gmail.com>
2024-09-12 01:49:30 -0400

    missing property

--- js/showdown-footnote.js
+++ js/showdown-footnote.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const footnote = {
+  type: `lang`
   filters: [
     {
-      type: 'lang',
+      type: `lang`,
       filter: function filter(text) {
         return text.replace(/^\[\^([\d\w]+)\]:\s*((\n+(\s{2,4}|\t).+)+)$/mg, function (str, name, rawContent, _, padding) {
           var content = converter.makeHtml(rawContent.replace(new RegExp('^' + padding, 'gm'), ''));
@@ -11,14 +12,14 @@ const footnote = {
         });
       }
     }, {
-      type: 'lang',
+      type: `lang`,
       filter: function filter(text) {
         return text.replace(/^\[\^([\d\w]+)\]:( |\n)((.+\n)*.+)$/mg, function (str, name, _, content) {
           return '<small class="footnote" id="footnote-' + name + '"><a href="#footnote-' + name + '"><sup>[' + name + ']</sup></a>: ' + content + '</small>';
         });
       }
     }, {
-      type: 'lang',
+      type: `lang`,
       filter: function filter(text) {
         return text.replace(/\[\^([\d\w]+)\]/m, function (str, name) {
           return '<a href="#footnote-' + name + '"><sup>[' + name + ']</sup></a>';
@@ -28,5 +29,5 @@ const footnote = {
   ],
 };
 
-showdown.extension('footnote', () => footnote.filters);
+showdown.extension(`footnote`, () => footnote.filters);
 
